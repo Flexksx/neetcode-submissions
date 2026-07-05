d467315d5d8e794eb38a2d24eceaf00b65781e5a
Cristian Cretu <cristian.cretu2808@gmail.com>
2026-07-05 15:54:19 +0300

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.java
@@ -0,0 +1,36 @@
+class Solution {
+    private static final Character L_ROUND = '(';
+    private static final Character L_SQUARE = '[';
+    private static final Character L_CURLY = '{';
+
+    private static final Character R_ROUND = ')';
+    private static final Character R_SQUARE = ']';
+    private static final Character R_CURLY = '}';
+
+    private static final Set<Character> OPENING_PARENS = Set.of(L_ROUND, L_SQUARE, L_CURLY);
+
+    private static final Map<Character, Character> CLOSING_TO_OPENING_PAREN =
+        Map.of(R_ROUND, L_ROUND, R_SQUARE, L_SQUARE, R_CURLY, L_CURLY);
+
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character current = s.charAt(i);
+
+            if (OPENING_PARENS.contains(current)) {
+                stack.addFirst(current);
+            } else if (stack.isEmpty()) {
+                return false;
+            }
+
+            else if (!CLOSING_TO_OPENING_PAREN.get(current).equals(stack.getFirst())) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
